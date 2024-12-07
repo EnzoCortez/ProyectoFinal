50e51063786e8c54130bbc6953180259b316eb18
Enzo Cortez <enzo.cortez@udla.edu.ec>
2024-12-07 17:58:25 -0500

    Cracion de modelos y vistas

---
+++ ProyectoFinal/Models/Estudiante.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoFinal.Models
+{
+    public class Estudiante
+    { 
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string CI { get; set; }
+        public string Carrera { get; set; }
+    }
+}
---
+++ ProyectoFinal/Services/FileServices.cs
@@ -0,0 +1,24 @@
+﻿using System.Text.Json;
+using ProyectoFinal.Models;
+
+namespace ProyectoFinal.Services
+{
+    public static class FileService
+    {
+        private static readonly string FilePath = Path.Combine(FileSystem.AppDataDirectory, "data.json");
+
+        public static async Task<List<Estudiante>> LoadEstudiantesAsync()
+        {
+            if (!File.Exists(FilePath)) return new List<Estudiante>();
+
+            var json = await File.ReadAllTextAsync(FilePath);
+            return JsonSerializer.Deserialize<List<Estudiante>>(json) ?? new List<Estudiante>();
+        }
+
+        public static async Task SaveEstudiantesAsync(List<Estudiante> estudiantes)
+        {
+            var json = JsonSerializer.Serialize(estudiantes, new JsonSerializerOptions { WriteIndented = true });
+            await File.WriteAllTextAsync(FilePath, json);
+        }
+    }
+}
